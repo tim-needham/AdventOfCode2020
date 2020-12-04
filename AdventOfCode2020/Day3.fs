7cf2c08c6f29539bce86281fe4c91eb7687c3e59
tneedham <tneedham@travelrepublic.co.uk>
2020-12-04 16:19:00 +0000

    Day 3

--- AdventOfCode2020/Day3.fs
+++ AdventOfCode2020/Day3.fs
@@ -4,18 +4,45 @@ open System;
 open System.Diagnostics;
 open System.IO;
 
+let rec traverse (cs : char list) (p : int) (w : int) (h : int) (m : string list) : char list =
+    match m with
+    | x::xs ->  if h = 1 then
+                    traverse (x.[p]::cs) ((p+w) % x.Length) w h xs;
+                else                    
+                    let ys = if xs = [] then [] else xs |> List.skip (h-1)
+                    traverse (x.[p]::cs) ((p+w) % x.Length) w h ys;
+    | _ -> cs |> List.rev |> List.tail;
+
 let run (file : string, testMode : bool) =
 
     let w = new Stopwatch();
     w.Start();
 
-    let input = Seq.toList(File.ReadLines(file))
-                |> List.map (fun x -> Int32.Parse(x.ToString()));
+    let test = [ "..##.......";
+                "#...#...#..";
+                ".#....#..#.";
+                "..#.#...#.#";
+                ".#...##..#.";
+                "..#.##.....";
+                ".#.#.#....#";
+                ".#........#";
+                "#.##...#...";
+                "#...##....#";
+                ".#..#...#.#" ];
 
-    0
+    let input = Seq.toList(File.ReadLines(file));
+
+    if testMode then test else input
+    |> traverse [] 0 3 1
+    |> List.filter(fun x -> x = '#')
+    |> List.length
     |> printfn "Day 3, part 1: %d";
 
-    0
+    [(1, 1); (3, 1); (5, 1); (7, 1); (1, 2)]
+    |> List.map (fun (x, y) -> traverse [] 0 x y (if testMode then test else input))
+    |> List.map (List.filter (fun x -> x = '#'))
+    |> List.map (List.length)
+    |> List.fold (fun a x -> a * int64 x) 1L
     |> printfn "Day 3, part 2: %d";
 
     w.Stop();
