25ef6d7eed780a17750b13a1b4297c38ec05dfaf
tneedham <tneedham@travelrepublic.co.uk>
2020-12-04 15:29:08 +0000

    Day 1

--- AdventOfCode2020/Day1.fs
+++ AdventOfCode2020/Day1.fs
@@ -4,6 +4,24 @@ open System;
 open System.Diagnostics;
 open System.IO;
 
+let rec pairs (l : 'a list) : seq<('a * 'a)> = 
+    seq { 
+        match l with
+        | h::t -> 
+            yield! t |> Seq.map (fun elem -> (h, elem))
+            yield! t |> pairs
+        | _ -> ()
+    };
+
+let rec triples (l : 'a list) : seq<('a * 'a * 'a)> =
+    seq {
+        match l with
+        | h1::h2::t ->
+            yield! h2::t |> pairs |> Seq.map (fun (a, b) -> (h1, a, b))
+            yield! h2::t |> triples
+        | _ -> ()
+    };
+
 let run (file : string, testMode : bool) =
 
     let w = new Stopwatch();
@@ -12,10 +30,17 @@ let run (file : string, testMode : bool) =
     let input = Seq.toList(File.ReadLines(file))
                 |> List.map (fun x -> Int32.Parse(x.ToString()));
 
-    0
+    let (a, b) = input
+                |> pairs
+                |> Seq.find(fun (x, y) -> x + y = 2020)
+
+    (a * b)
     |> printfn "Day 1, part 1: %d";
 
-    0
+    let (c, d, e) = input
+                    |> triples
+                    |> Seq.find(fun (x, y, z) -> x + y + z = 2020)
+    (c * d * e)
     |> printfn "Day 1, part 2: %d";
 
     w.Stop();
