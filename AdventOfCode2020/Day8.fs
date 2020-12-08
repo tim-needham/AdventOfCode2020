0e082d4e4f77db22d3f506589407b7eca53d4c00
tneedham <tneedham@travelrepublic.co.uk>
2020-12-08 08:45:21 +0000

    Day 8

--- AdventOfCode2020/Day8.fs
+++ AdventOfCode2020/Day8.fs
@@ -4,18 +4,76 @@ open System;
 open System.Diagnostics;
 open System.IO;
 
+type Instruction =
+    | Acc of param : int
+    | Jmp of param : int
+    | Nop of param : int
+;;
+
+let parse (s : string) : Instruction =
+    match s.Split([| " " |], StringSplitOptions.RemoveEmptyEntries) with
+    | [| "acc"; n |] -> Acc (Int32.Parse(n));
+    | [| "jmp"; n |] -> Jmp (Int32.Parse(n));
+    | [| "nop"; n |] -> Nop (Int32.Parse(n));
+    | _ -> failwithf "Unrecognised input %s" s;
+
+
+let execute (a : int) (p : int) (i : Instruction) : int * int =
+    match i with
+    | Acc x -> (a+x, p+1);
+    | Jmp x -> (a, p+x);
+    | Nop _ -> (a, p+1);
+
+let rec simulate (a : int) (p : int) (ps : int list) (is : Instruction list) : bool * int =
+    if p >= is.Length then
+        (true, a);
+    elif List.contains p ps then
+        (false, a);
+    else
+        let (a', p') = execute a p is.[p];
+        simulate a' p' (p::ps) is;
+
+let rec fix (p : int) (is : Instruction list) =
+    if p >= is.Length then
+        failwith "Ran out of instructions to change";
+    
+    match is.[p] with
+    | Acc _ ->  fix (p+1) is;
+    | Jmp x ->  let (a, b) = is
+                                |> List.mapi (fun i j -> if i=p then Nop x else j)
+                                |> simulate 0 0 [];
+                if a then b else fix (p+1) is;                
+    | Nop x ->  let (a, b) = is
+                                |> List.mapi (fun i j -> if i=p then Jmp x else j)
+                                |> simulate 0 0 [];
+                if a then b else fix (p+1) is;
+
 let run (file : string, testMode : bool) =
 
     let w = new Stopwatch();
     w.Start();
 
-    let input = Seq.toList(File.ReadLines(file))
-                |> List.map (fun x -> Int32.Parse(x.ToString()));
+    let test = ["nop +0";
+                "acc +1";
+                "jmp +4";
+                "acc +3";
+                "jmp -3";
+                "acc -99";
+                "acc +1";
+                "jmp -4";
+                "acc +6" ]
+                |> List.map parse;
 
-    0
+    let input = Seq.toList(File.ReadLines(file))
+                |> List.map parse;
+
+    if testMode then test else input
+    |> simulate 0 0 []
+    |> snd
     |> printfn "Day 8, part 1: %d";
 
-    0
+    if testMode then test else input
+    |> fix 0
     |> printfn "Day 8, part 2: %d";
 
     w.Stop();
