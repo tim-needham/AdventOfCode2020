6529fd4e4ac28718f31202b88293e850acf25bdd
tneedham <tneedham@travelrepublic.co.uk>
2020-12-09 10:59:09 +0000

    Day 9 - tidy

--- AdventOfCode2020/Day9.fs
+++ AdventOfCode2020/Day9.fs
@@ -57,26 +57,7 @@ let run (file : string, testMode : bool) =
     let w = new Stopwatch();
     w.Start();
 
-    let test = [ 35L;
-                20L;
-                15L;
-                25L;
-                47L;
-                40L;
-                62L;
-                55L;
-                65L;
-                95L;
-                102L;
-                117L;
-                150L;
-                182L;
-                127L;
-                219L;
-                299L;
-                277L;
-                309L;
-                576L ];
+    let test = [ 35L; 20L; 15L; 25L; 47L; 40L; 62L; 55L; 65L; 95L; 102L; 117L; 150L; 182L; 127L; 219L; 299L; 277L; 309L; 576L ];
 
     let input = Seq.toList(File.ReadLines(file))
                 |> List.map (fun x -> Int64.Parse(x.ToString()));
