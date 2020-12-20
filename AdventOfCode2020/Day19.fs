cbe1e3c8b29369aa31a565ee6e88d8f4138fbe92
tneedham <tneedham@travelrepublic.co.uk>
2020-12-20 16:39:24 +0000

    Day 19 - tidier

--- AdventOfCode2020/Day19.fs
+++ AdventOfCode2020/Day19.fs
@@ -43,7 +43,7 @@ let rec parse (b : bool) (ss : string list) : Rule list * char list list =
     | x::xs ->  let (ps, qs) = parse b xs;
                 if b then ((parseRule x)::ps, qs) else (ps, (parseInput x)::qs);
 
-let rec stackRules (rs : int list) (m : Map<int, RuleType>) (cs : char list) =
+let rec stackRules (rs : int list) (m : Map<int, RuleType>) (cs : char list) : bool =
     match rs, cs with
     | [], [] -> true;
     | _, [] -> false;
@@ -53,7 +53,7 @@ let rec stackRules (rs : int list) (m : Map<int, RuleType>) (cs : char list) =
                         | Sequence ss -> stackRules (ss@xs) m cs;
                         | Adjunct (ps, qs) -> if stackRules (ps@xs) m cs then true else stackRules (qs@xs) m cs;
 
-let matchRules (rs : Rule list) (cs : char list) =
+let matchRules (rs : Rule list) (cs : char list) : bool =
     let m = rs
             |> List.map (fun r -> (r.Id, r.Type))
             |> Map.ofList
@@ -79,53 +79,7 @@ let run (file : string, testMode : bool) =
                 "aaaabbb" ]
                 |> parse true;
 
-    let test2 = [ "42: 9 14 | 10 1";
-                "9: 14 27 | 1 26";
-                "10: 23 14 | 28 1";
-                "1: \"a\"";
-                "11: 42 31";
-                "5: 1 14 | 15 1";
-                "19: 14 1 | 14 14";
-                "12: 24 14 | 19 1";
-                "16: 15 1 | 14 14";
-                "31: 14 17 | 1 13";
-                "6: 14 14 | 1 14";
-                "2: 1 24 | 14 4";
-                "0: 8 11";
-                "13: 14 3 | 1 12";
-                "15: 1 | 14";
-                "17: 14 2 | 1 7";
-                "23: 25 1 | 22 14";
-                "28: 16 1";
-                "4: 1 1";
-                "20: 14 14 | 1 15";
-                "3: 5 14 | 16 1";
-                "27: 1 6 | 14 18";
-                "14: \"b\"";
-                "21: 14 1 | 1 14";
-                "25: 1 1 | 1 14";
-                "22: 14 14";
-                "8: 42";
-                "26: 14 22 | 1 20";
-                "18: 15 15";
-                "7: 14 5 | 1 21";
-                "24: 14 1";
-                "";
-                "abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa";
-                "bbabbbbaabaabba";
-                "babbbbaabbbbbabbbbbbaabaaabaaa";
-                "aaabbbbbbaaaabaababaabababbabaaabbababababaaa";
-                "bbbbbbbaaaabbbbaaabbabaaa";
-                "bbbababbbbaaaaaaaabbababaaababaabab";
-                "ababaaaaaabaaab";
-                "ababaaaaabbbaba";
-                "baabbaaaabbaaaababbaababb";
-                "abbbbabbbbaaaababbbbbbaaaababb";
-                "aaaaabbaabaaaaababaa";
-                "aaaabbaaaabbaaa";
-                "aaaabbaabbaaaaaaabbbabbbaaabbaabaaa";
-                "babaaabbbaaabaababbaabababaaab";
-                "aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba" ]
+    let test2 = Seq.toList(File.ReadAllLines("test19.txt"))
                 |> parse true;
 
     let input = Seq.toList(File.ReadLines(file))
